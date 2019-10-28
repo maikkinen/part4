8072149ebe47df69bb046d137ce272a425efe790
Mai <anna-maija.rauramaa@aalto.fi>
2019-10-28 16:42:24 +0200

    Teht_4.2 complete.

--- bloglist/controllers/app.js
+++ bloglist/controllers/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogposts')
-//const middleware = require('.utils/middleware')
+const middleware = require('.utils/middleware')
 const mongoose = require('mongoose')
 
 
@@ -21,7 +21,11 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
 app.use(cors())
 app.use(express.static('build'))
 app.use(bodyParser.json())
+app.use(middleware.requestLogger)
 
 app.use('api/notes', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
