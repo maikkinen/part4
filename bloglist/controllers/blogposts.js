539f4bd532e519a8c1f10d6fe198b5bcfa72ca19
Mai <anna-maija.rauramaa@aalto.fi>
2019-11-12 17:12:18 +0200

    Teht_4.19 & 4.20 completed

--- bloglist/controllers/blogposts.js
+++ bloglist/controllers/blogposts.js
@@ -1,44 +1,66 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogpost')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({})
     .populate('user', { username: 1, name: 1 })
-
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user._id,
-  })
+  const token = request.token
 
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id,
+    })
+
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
-    response.json(savedBlog.toJSON())
-  } catch(exception) {
+    response.status(201).json(savedBlog.toJSON())
+  } catch (exception) {
     next(exception)
   }
 })
 
 blogsRouter.delete('/:id', async (request, response, next) => {
   try {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+    const token = request.token
+
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    //decodedToken.id === request.params.userId
+    //decoded
+
+    const blogToBeRemoved = await Blog.findById(request.params.id)
+
+    if (blogToBeRemoved.user.toString() === decodedToken.id.toString()) {
+
+      await Blog.findByIdAndRemove(request.params.id)
+      response.status(204).end()
+    }
   } catch (exception) {
-    next (exception)
+    next(exception)
   }
 })
 
@@ -59,7 +81,7 @@ blogsRouter.put('/:id', async (request, response, next) => {
     await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
     response.json(blog.toJSON)
   } catch (exception) {
-    next (exception)
+    next(exception)
   }
 
 })
--- bloglist/utils/middleware.js
+++ bloglist/utils/middleware.js
@@ -10,6 +10,19 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = (request, response, next) => {
+  const getTokenFrom = request => {
+    console.log(request)
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+  }
+  request.token = getTokenFrom(request)
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
@@ -17,6 +30,10 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({
+      error: 'invalid token'
+    })
   }
 
   next(error)
@@ -26,5 +43,6 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
